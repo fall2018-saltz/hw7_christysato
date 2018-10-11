6ba97af6c8e214c0c0cc02d81dcda848932b173b
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 11:15:25 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -1,1 +1,38 @@
 
+
+#named rawdata dataset as dfStates
+dfStates <- rawdata
+
+#STEPA1
+#created function to clean the dataframe
+MyMode <- function(dfStates)
+{
+  #removes first row because it is total for US, we do not need
+  dfStates <- dfStates[-1,]
+  #last row is Puerto Rico and should be removed
+  num.row <- nrow(dfStates)
+  dfStates <- dfStates[-num.row,]
+  #removes first 4 columns
+  dfStates <- dfStates[,-1:-4]
+  #changes the name of remaining columns
+  colnames(dfStates) <- c("stateName", "population", "popOver18","percentOver18")
+  return(dfStates)
+}
+dfStates <- MyMode(dfStates)
+#provides clean dataset
+str(dfStates)
+
+#STEPA2
+#copied USArrests into local variable called arrests
+arrests <- USArrests
+arrests <- data.frame(USArrests)
+#created a new column for state names
+arrests$stateName <- row.names(arrests)
+arrests
+
+
+#STEPA3
+#merge the two datasets with matching stateName column
+USArrestsdf <- merge(dfStates, arrests, by="stateName")
+
+head(USArrestsdf)
