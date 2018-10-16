9f5630b9e6c526462d015b5af5c44cdb62148925
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-16 17:20:59 -0400

    Saving Maps.R

--- Maps.R
+++ Maps.R
@@ -2,3 +2,7 @@
 library("ggmap")
 
 #STEPB3
+#once ggmap is installed, can get the map of the USA
+us <- map_data("state")
+#ggplot expects state names to be lowercase so function tolower will make everything lower case letters credit: "An Introduction to Data Science" pg 160
+USArrestsDF$stateName <- tolower(USArrestsDF$stateName)
