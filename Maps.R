5cb47fdd66b7c9ab638747cfd023c09fe7a518db
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 17:35:15 -0400

    Saving Maps.R

--- Maps.R
+++ Maps.R
@@ -12,7 +12,7 @@ mapcolor <- ggplot(USArrestsDF, aes(map_id = stateName))
 #fill it with state area
 str(USArrestsDF)
 USArrestsDF[1,]
-mapcolor <- mapcolor + geom_map(map= us, fill= state.area, color= "black")
+mapcolor <- mapcolor + geom_map(map= us, aes(fill= state.area, color= "black"))
 #make limits the x and y coordinates (longitude and latitude)
 mapcolor <- mapcolor + expand_limits(x= USArrestsDF$x, y= USArrestsDF$y)
 #named the map Area of States
